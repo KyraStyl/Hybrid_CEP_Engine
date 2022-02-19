ddbb8f0361f33fc9f56eac2a8a5b22788084b65f
kyrastyl <kyrastyl@csd.auth.gr>
2022-02-19 14:01:54 +0200

    add the implementation - v01
    first version of the code, hybrid engine chosen by command line arguments

---
+++ implementation/code/src/main/java/cetscommon/events/Event.java
@@ -0,0 +1,45 @@
+package cetscommon.events;
+
+import cetscommon.common.values.Value;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Event implements Serializable {
+  public final long timestamp;
+  protected Value[] values;
+
+  public Event(long timestamp, Value[] values) {
+    this.timestamp = timestamp;
+    this.values = values;
+  }
+
+  public Value get(int i) {
+    return values[i];
+  }
+
+  public int size() {return values.length;}
+
+  @Override
+  public String toString() {
+    return "Event{" + timestamp
+        + ", " + Arrays.toString(values)
+        + '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Event event = (Event) o;
+    return timestamp == event.timestamp && Arrays.equals(values, event.values);
+  }
+
+  @Override
+  public int hashCode() {
+    int result = Objects.hash(timestamp);
+    result = 31 * result + Arrays.hashCode(values);
+    return result;
+  }
+}
