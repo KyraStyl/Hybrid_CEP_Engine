c5f7c97dfcc8b4fe044ee7ae61786fdb211a22b2
kyrastyl <kyrastyl@csd.auth.gr>
2022-03-10 20:05:38 +0200

    fixed check generator

--- datasets/generators/checkGen.py
+++ datasets/generators/checkGen.py
@@ -132,5 +132,5 @@ if __name__ == "__main__":
         transform(filename)
         filename +="-transf"
     if extend:
-        produce(filename, prob, 1000)
+        produce(filename, prob, options["events"])
         
